864e3a82f4a949d17c86a3831c551d54f915dcc1
Donald Hobern <dhobern@gmail.com>
2020-09-28 17:27:51 +1000

    Local build script for constructing HTML and ZIP versions

--- build.bat
+++ build.bat
@@ -1,5 +1,11 @@
-java -jar ../CoLDPUtils/target/CoLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar Pterophoroidea coldp/data
+java -cp ../CoLDPUtils/target/CoLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.CoLDPTool -t Pterophoroidea -i 4 -T ../hobern.net/Pterophoroidea/index.php -e Pterophoroidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Pterophoroidea-TEST.html
+
+java -cp ../CoLDPUtils/target/CoLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.CoLDPTool -t Pterophoroidea -i 4 -xo ../hobern.net/Pterophoroidea/Pterophoroidea-catalogue.html coldp/data
+
 erase Pterophoroidea.zip
 cd coldp
 7z a ../Pterophoroidea.zip *
 cd ..
+
+copy Pterophoroidea.zip ..\hobern.net\Pterophoroidea
+
