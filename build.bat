1c9b01e925e8ab50268b3a3bf40769c15ee0eadb
Donald Hobern <dhobern@gmail.com>
2020-09-29 15:32:33 +1000

    Revised build process

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-java -cp ../CoLDPUtils/target/CoLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.CoLDPTool -t Pterophoroidea -i 4 -T ../hobern.net/Pterophoroidea/index.php -e Pterophoroidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Pterophoroidea-TEST.html
+java -cp ../COLDPUtils/target/COLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.COLDPTool TOHTML -t Pterophoroidea -i 4 -T ../hobern.net/Pterophoroidea/index.php -e Pterophoroidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Pterophoroidea-TEST.html
 
-java -cp ../CoLDPUtils/target/CoLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.CoLDPTool -t Pterophoroidea -i 4 -xo ../hobern.net/Pterophoroidea/Pterophoroidea-catalogue.html coldp/data
+java -cp ../COLDPUtils/target/COLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.COLDPTool TOHTML -t Pterophoroidea -i 4 -xo ../hobern.net/Pterophoroidea/Pterophoroidea-catalogue.html coldp/data
 
 erase Pterophoroidea.zip
 cd coldp
