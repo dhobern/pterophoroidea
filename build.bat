64da03f07be27c31c9c7c3a8b428c4ad09a19ef6
Donald Hobern <dhobern@gmail.com>
2020-09-30 12:20:20 +1000

    New source for template file, no echo

--- build.bat
+++ build.bat
@@ -1,7 +1,4 @@
-java -cp ../COLDPUtils/target/COLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.COLDPTool TOHTML -t Pterophoroidea -i 4 -T ../hobern.net/Pterophoroidea/index.php -e Pterophoroidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Pterophoroidea-TEST.html
-
-java -cp ../COLDPUtils/target/COLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.COLDPTool TOHTML -t Pterophoroidea -i 4 -xo ../hobern.net/Pterophoroidea/Pterophoroidea-catalogue.html coldp/data
-
+@ECHO OFF
 erase Pterophoroidea.zip
 cd coldp
 7z a ../Pterophoroidea.zip *
@@ -9,3 +6,6 @@ cd ..
 
 copy Pterophoroidea.zip ..\hobern.net\Pterophoroidea
 
+.\coldp.bat TOHTML -t Pterophoroidea -i 4 -T ../hobern.net/Pterophoroidea/catalogue.php -e Pterophoroidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Pterophoroidea-TEST.html
+
+.\coldp.bat TOHTML -t Pterophoroidea -i 4 -xo ../hobern.net/Pterophoroidea/Pterophoroidea-catalogue.html coldp/data
