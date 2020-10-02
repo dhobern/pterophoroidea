1544955cc1e907e65f2a4eefb848e42e8709e517
Donald Hobern <dhobern@gmail.com>
2020-10-02 16:36:17 +1000

    Rename -i parameter to -w

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ cd ..
 
 copy Pterophoroidea.zip ..\hobern.net\Pterophoroidea
 
-.\coldp.bat TOHTML -t Pterophoroidea -i 4 -T ../hobern.net/Pterophoroidea/catalogue.php -e Pterophoroidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Pterophoroidea-TEST.html
+.\coldp.bat TOHTML -t Pterophoroidea -w 4 -T ../hobern.net/Pterophoroidea/catalogue.php -e Pterophoroidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Pterophoroidea-TEST.html
 
-.\coldp.bat TOHTML -t Pterophoroidea -i 4 -xo ../hobern.net/Pterophoroidea/Pterophoroidea-catalogue.html coldp/data
+.\coldp.bat TOHTML -t Pterophoroidea -w 4 -xo ../hobern.net/Pterophoroidea/Pterophoroidea-catalogue.html coldp/data
