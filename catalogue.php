b914864ca6c6c68c9ff2400657d6fd69d1f767be
Donald Hobern <dhobern@gmail.com>
2022-11-04 02:49:41 +1100

    Pakistan

--- catalogue.php
+++ catalogue.php
@@ -30,7 +30,7 @@
         </header>
         
         <div class="Overview">
-            <h2>Catalogue of the Pterophoroidea of the World, version 1.1.22.307 (03 Nov 2022)</h2>
+            <h2>Catalogue of the Pterophoroidea of the World, version 1.1.22.308 (04 Nov 2022)</h2>
             <p><a href="index.php">See the <strong>background to this catalogue</strong> (including search and browse function)</a></p>
         </div>
         
