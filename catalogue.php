29477ba028c7d3b43e168eadf4c4e63a755a4c31
Donald Hobern <dhobern@gmail.com>
2024-08-02 18:30:51 +1000

    Ecuador 3

--- catalogue.php
+++ catalogue.php
@@ -30,7 +30,7 @@
         </header>
         
         <div class="Overview">
-            <h2>Catalogue of the Pterophoroidea of the World, version 1.1.24.199 (17 Jul 2024)</h2>
+            <h2>Catalogue of the Pterophoroidea of the World, version 1.1.24.215 (02 Aug 2024)</h2>
             <p><a href="index.php">See the <strong>background to this catalogue</strong> (including search and browse function)</a></p>
         </div>
         
